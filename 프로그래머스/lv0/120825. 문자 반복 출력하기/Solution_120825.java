6de2043b4aab7f0b8adf47be4dff275736acd776
jbb4223 <jbb4223@3top.co.kr>
2023-03-13 21:41:25 +0900

    프로그래머스 인텔리제이에서 자바파일 빌드 처리 확인용

---
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/Solution_120825.java
@@ -0,0 +1,22 @@
+public class Solution_120825 {
+
+    public static void main(String[] args) {
+        // 예시
+        System.out.println("=======================");
+        System.out.println(solution("hello",3));
+        System.out.println("=======================");
+    }
+
+    public static String solution(String my_string, int n) {
+        String answer = "";
+        char [] arr = my_string.toCharArray();
+
+        for(int i=0; i<arr.length; i++) {
+            for(int j=0; j<n; j++) {
+                answer += arr[i];
+            }
+        }
+
+        return answer;
+    }
+}
