72f185d2d71d3250282cbbf86d7d1d6c3451f9dd
jbb4223 <jbb4223@3top.co.kr>
2023-03-13 21:16:38 +0900

    프로그래머스 인텔리제이에서 자바파일 빌드 처리 확인용

--- 프로그래머스/lv0/120813. 짝수는 싫어요/Solution_120813.java
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/Solution_120813.java
@@ -1,5 +1,15 @@
-class Solution {
-    public int[] solution(int n) {
+import java.util.Arrays;
+
+public class Solution_120813 {
+
+    public static void main(String[] args) {
+        // 예시
+        System.out.println("=======================");
+        System.out.println(Arrays.toString(solution(10)));
+        System.out.println("=======================");
+    }
+
+    public static int[] solution(int n) {
         int a = 0;
         int[] answer;
         
