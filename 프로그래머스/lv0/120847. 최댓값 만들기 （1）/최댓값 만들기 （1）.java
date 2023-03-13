6e3f1575642504abb01d80be018b22246f0ec8d1
Park Jin Boung <70303921+jbb4223@users.noreply.github.com>
2023-03-13 09:03:10 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.33 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        
+        // numbers int 배열 오름차순으로 정렬
+        Arrays.sort(numbers);
+        
+        answer = numbers[numbers.length -1] * numbers[numbers.length -2]; 
+        
+        return answer;
+    }
+}
