0b0f1ad272663d3e0e93714aaf39100a98e1345c
jbb4223 <jbb4223_3top@gsretail.com>
2023-03-15 17:23:00 +0900

    Revert "git revert 테스트"
    This reverts commit e1a5b9b46757939f88374f1b0d0de77342111b87.
    
    # Conflicts:
    #	프로그래머스/lv0/120583. 중복된 숫자 개수/Solution_120583.java

--- 프로그래머스/lv0/120583. 중복된 숫자 개수/Solution_120583.java
+++ 프로그래머스/lv0/120583. 중복된 숫자 개수/Solution_120583.java
@@ -7,7 +7,7 @@ public class Solution_120583 {
         System.out.println(solution(arr,1));
         System.out.println("=======================");
     }
-    /// git revert testing2
+    /// git revert test
 
     public static int solution(int[] array, int n) {
         int answer = 0;
