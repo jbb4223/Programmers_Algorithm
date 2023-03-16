48bb2280aa7eca969324073a70402680cd335537
jbb4223 <jbb4223_3top@gsretail.com>
2023-03-16 16:34:33 +0900

    프로그래머스 인텔리제이에서 자바파일 빌드 처리 확인용

--- 프로그래머스/lv0/120893. 대문자와 소문자/Solution_120893.java
+++ 프로그래머스/lv0/120893. 대문자와 소문자/Solution_120893.java
@@ -1,5 +1,13 @@
-class Solution {
-    public String solution(String my_string) {
+public class Solution_120893 {
+
+    public static void main(String[] args) {
+        // 예시
+        System.out.println("=======================");
+        System.out.println(solution("cccCCC"));
+        System.out.println("=======================");
+    }
+
+    public static String solution(String my_string) {
         StringBuilder answer = new StringBuilder();
         char [] charArr = my_string.toCharArray();
 
