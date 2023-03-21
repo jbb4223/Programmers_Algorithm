11dd15dd00f6dff1ef316fa55f9b1f9d932a09ce
Park Jin Boung <70303921+jbb4223@users.noreply.github.com>
2023-03-21 13:52:05 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 67.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int piece = 1;
+
+        while((n*piece) % 6 != 0) {
+            piece++;
+        }
+
+        answer = (n*piece) / 6;
+
+        return answer;
+    }
+}
