4b9ef00621c7d9068c734d02a8e9d8eb42b4479e
jbb4223 <jbb4223_3top@gsretail.com>
2023-03-27 15:18:47 +0900

    프로그래머스 인텔리제이에서 자바파일 빌드 처리 확인용

--- 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/Solution_120840.java
+++ 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/Solution_120840.java
@@ -1,4 +1,13 @@
-class Solution {
+public class Solution_120840 {
+
+    public static void main(String[] args) {
+        // 예시
+        int [] arr = {1, 2, 3};
+        System.out.println("=======================");
+        System.out.println(solution(3,2));
+        System.out.println("=======================");
+    }
+
     public static int comb(int balls, int share) {
         if (balls == share || share == 0) return 1;
         return solution((balls - 1), (share - 1)) + solution(balls - 1, share);
