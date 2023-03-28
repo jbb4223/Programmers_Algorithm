c7a7455ee6d089c974102894c9dfedebe8579e2c
jbb4223 <jbb4223_3top@gsretail.com>
2023-03-28 15:25:23 +0900

    프로그래머스 인텔리제이에서 자바파일 빌드 처리 확인용

--- 프로그래머스/lv0/120860. 직사각형 넓이 구하기/Solution_120860.java
+++ 프로그래머스/lv0/120860. 직사각형 넓이 구하기/Solution_120860.java
@@ -1,6 +1,17 @@
 import java.util.Arrays;
-class Solution {
-    public int solution(int[][] dots) {
+
+public class Solution_120860 {
+
+    public static void main(String[] args) {
+        // 예시
+        int [][] arr = {{-1, -1}, {1, 1}, {1, -1}, {-1, 1}};
+//            int [] arr2 = {5, 5};
+        System.out.println("=======================");
+        System.out.println(solution(arr));
+        System.out.println("=======================");
+    }
+
+    public static int solution(int[][] dots) {
             int answer = 0;
             int [] xArr = new int[dots.length];
             int [] yArr = new int[dots.length];
