6a249a1e5201ff0ba439ac0ad461fe68c5356598
jbb4223 <jbb4223_3top@gsretail.com>
2023-03-28 17:07:42 +0900

    유한소수 판별하기 다시보기

--- 프로그래머스/lv0/test.java
+++ 프로그래머스/lv0/test.java
@@ -1,34 +1,38 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
-public class test {
+    public class test {
 
-    public static void main(String[] args) {
-        // 예시
-        int [] arr = {1, 2, 3, 4, 5, 6, 7};
-        System.out.println("=======================");
-        System.out.println(Arrays.toString(solution(12)));
-        System.out.println("=======================");
-    }
-
-    public static int[] solution(int n) {
-        int[] answer = {};
-        List<Integer> array = new ArrayList<>();
-
-        int idx = 2;
-        while (n > 1) {
-            if (n % idx == 0) {
-                if (!array.contains(idx)) {
-                    array.add(idx);
-                }
-                n /= idx;
-            } else {
-                idx++;
-            }
+        public static void main(String[] args) {
+            // 예시
+            String [] arr = {"meosseugi", "1234"};
+//            int [] arr2 = {5, 5};
+            // 1 2 3 4 6 12
+            System.out.println("=======================");
+            System.out.println(solution(12,100));
+            System.out.println("=======================");
         }
 
-        answer = array.stream().distinct().mapToInt(Integer::intValue).toArray();
-        return answer;
-    }
+        public static int solution(int a, int b) {
+            int finalB = b / GCD(a, b);
+
+            while( finalB != 1) {
+                if(finalB % 2 == 0) {
+                    finalB /= 2;
+                }else if (finalB % 5 == 0) {
+                    finalB /= 5;
+                }else {
+                    return 2;
+                }
+            }
+
+            return 1;
+        }
+        // 유클리드 호제법!! 두 수의 최대 공약수 구하기
+        private static int GCD(int a, int b) {
+            if (b == 0) {
+                return a;
+            } else {
+                return GCD(b, a % b);
+            }
+        }
 }
